43463f74f1131d1344174965b78518d219e82b7c
Rahul-Patil123 <rahulpatilccis@gmail.com>
2025-09-09 23:45:43 +0530

    Formatting

--- time-till-deadline.py
+++ time-till-deadline.py
@@ -1,11 +1,10 @@
-import datetime
-
+from  datetime import datetime
 user_input = input("Enter your goal with a deadline seperated by colon:\n")
 input_list = user_input.split(":")
 goal = input_list[0]
 deadline = input_list[1]
 
-deadline_date = datetime.datetime.strptime(deadline,"%d.%m.%Y")
-today_date = datetime.datetime.today()
+deadline_date = datetime.strptime(deadline,"%d.%m.%Y")
+today_date = datetime.today()
 time_remaining = deadline_date - today_date
-print(f"Time remaining till your deadline is : {time_remaining.days}")
+print(f"Days remaining till your deadline is : {time_remaining.days} days")
