d5f3c8c875f43f8e121020d931340ad58cc140a6
Rahul-Patil123 <rahulpatilccis@gmail.com>
2025-09-10 23:44:09 +0530

    Done

--- time-till-deadline.py
+++ time-till-deadline.py
@@ -3,7 +3,6 @@ user_input = input("Enter your goal with a deadline seperated by colon:\n")
 input_list = user_input.split(":")
 goal = input_list[0]
 deadline = input_list[1]
-
 deadline_date = datetime.strptime(deadline,"%d.%m.%Y")
 today_date = datetime.today()
 time_remaining = deadline_date - today_date
