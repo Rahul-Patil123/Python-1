e24d0b099bcd88d70d1bedc72e55455d9664b35f
Rahul-Patil123 <rahulpatilccis@gmail.com>
2025-09-13 23:11:18 +0530

    Inventory Size

--- main.py
+++ main.py
@@ -19,9 +19,10 @@ print("These are total products per company:", product_per_company)
 for product_row in range(2, product_details.max_row + 1):
     supplier_name = product_details.cell(product_row, 4).value
     inventory_value = product_details.cell(product_row, 2).value
+    inventory_price = product_details.cell(product_row, 3).value
     if supplier_name in inventory_per_company:
-        inventory_per_company[supplier_name] += inventory_value
+        inventory_per_company[supplier_name] += inventory_value * inventory_price
     else:
-        inventory_per_company[supplier_name] = inventory_value
+        inventory_per_company[supplier_name] = inventory_value * inventory_price
 
 print("These are total inventory per company:", inventory_per_company)
