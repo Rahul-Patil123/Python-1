35f7f9ca43ae24462e2f57fbbb4492c20734913d
Rahul-Patil123 <rahulpatilccis@gmail.com>
2025-09-15 23:32:34 +0530

    Classes and Objects

---
+++ user.py
@@ -0,0 +1,12 @@
+class User:
+    def __init__(self,email,name,password,current_job_title):
+        self.email = email
+        self.name = name
+        self.password = password
+        self.current_job_title = current_job_title
+    def change_password():
+        #do_something
+        print("Change Password")
+    def change_job_title():
+        #do_something
+        print("Change Password")
