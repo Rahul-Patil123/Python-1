ed760de47b4463aca22188083477c62c07de07c6
Rahul-Patil123 <rahulpatilccis@gmail.com>
2025-09-16 23:35:35 +0530

    Try OOPS

--- oops.py
+++ oops.py
@@ -2,7 +2,5 @@ from user import User
 from post import Post
 rahul = User("rahulpatilccis@gmail.com","Rahul Ganeshwar Patil", "changeme", "SRE")
 rahul.get_user_details()
-rahul.change_job_title("DevOps Engineer")
-rahul.get_user_details()
 new_post = Post("I have successfully learned Classes and objects in python", rahul.name)
 new_post.get_post()
